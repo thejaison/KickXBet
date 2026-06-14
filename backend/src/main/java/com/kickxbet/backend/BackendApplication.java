2788e12f1534d3fa7f28e22d4bfcf2c8eced684e
thejaison <joyaljaison75@gmail.com>
2026-06-14 11:00:34 +0530

    Update backend

--- backend/src/main/java/com/kickxbet/backend/BackendApplication.java
+++ backend/src/main/java/com/kickxbet/backend/BackendApplication.java
@@ -2,6 +2,9 @@ package com.kickxbet.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -10,4 +13,16 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+						.allowedHeaders("*");
+			}
+		};
+	}
 }
